3f0e462298fb8c765087b3073c5aa8c5eb5da9a6
kleekitz <cloverkid13@gmail.com>
2019-12-27 15:10:12 -0500

    Now makes the selection a link. There is error checking to prevent a non-Jira type link from opening.
    Next steps: Figure how to stop the context menu from showing up at all UNLESS it's a Jira-type.

--- JiraLink.js
+++ JiraLink.js
@@ -1,6 +1,10 @@
+const preurl = "https://optanix.atlassian.net/browse/";
+var fullurl = "";
+var regex = new RegExp('([A-Z]|[a-z]){1,5}-([0-9]{1,5})');
+
 browser.contextMenus.create({
     id: "gotoJira",
-    title: "Open as Jira Page",
+    title: "Open as Jira Page: %s",
     contexts: ["selection"]
 });
 
@@ -8,4 +12,21 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
   console.log("Item " + info.menuItemId + " clicked " +
               "in tab " + tab.id);
   console.log(info.selectionText);
+  
+  const selText = info.selectionText;
+
+  if ( regex.test(selText) ) {
+  	fullurl = preurl + selText;
+  } else {
+  	browser.contextMenus.remove(info.menuItemId);
+  	throw new Error("Error! \"" + selText + "\" is not a valid Jira issue type!");
+  }
+
+
+  
+
+  browser.tabs.create({
+  	url:fullurl
+  });
+  console.log(fullurl);
 });
